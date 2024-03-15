eee56e2aa78e21520cdf176c69dfa96b4ab20c22
Hleb Nikalayenka <s28311@pjwstk.edu.pl>
2024-03-15 16:21:06 +0100

    5. Add check into class to check if end index is before start

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def e_squares(start, end):
 
 class SquareGenerator:
     def generate(self, start, end):
+        if end < start:
+            raise IndexError("End index should be after start index")
         return [x*x for x in range(start, end + 1)]
 
 generator = SquareGenerator()
