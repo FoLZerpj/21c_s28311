ee032201aaf508b56f98c2e37afaf491c4592ff1
Hleb Nikalayenka <s28311@pjwstk.edu.pl>
2024-03-15 16:19:42 +0100

    3. Add class to generate list from range

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ l = [x*x for x in range(1, 11)]
 
 def e_squares(start, end):
     return [x*x for x in range(start, end + 1)]
+
+class SquareGenerator:
+    def generate(self, start, end):
+        return [x*x for x in range(start, end + 1)]
